f4a94e4b46ea242524525ac5eceacd10fbdb6640
Anıl Kaynar <aanilkay@gmail.com>
2017-10-12 18:14:42 +0300

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -25,6 +25,16 @@ public class Recursion {
     public String notstatic="I am not a static variable";
     public static int bol=14;
     public int digeryirmisek;
+    public boolean has77(int[] nums) {
+  for(int i=0;i<nums.length-1;i++){
+    if(nums[i]==7 && nums[i+1]==7)return true;
+  }
+  for(int i=0;i<nums.length-2;i+=1){
+    if(nums[i]==7 && (nums[i+2]==7)){return true;}
+  }
+  return false;
+}
+
     public String mixString(String a, String b) {
   StringBuilder c=new StringBuilder();
   int i=0;
